5715919399f88284b6cbd4e5d804ebe9cae6c339
adam <adamyusuf.ay@gmail.com>
2020-05-19 01:22:15 +0100

    Updates

---
+++ precache-manifest.73964f3eb212bd61c19cfe7c56f53483.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "932770b7be487645b174a46aa9cc3a9f",
+    "url": "/AAYusuf/myShop.git/index.html"
+  },
+  {
+    "revision": "7639abee3a9de83fba0f",
+    "url": "/AAYusuf/myShop.git/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1093c6673be7d85f2a49",
+    "url": "/AAYusuf/myShop.git/static/js/2.468dcf52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AAYusuf/myShop.git/static/js/2.468dcf52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7639abee3a9de83fba0f",
+    "url": "/AAYusuf/myShop.git/static/js/main.4a793b81.chunk.js"
+  },
+  {
+    "revision": "f2b488ecdc7fc82c5b2d",
+    "url": "/AAYusuf/myShop.git/static/js/runtime-main.eefd86ff.js"
+  }
+]);
